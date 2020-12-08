87b03433f1cd54be5baeae9cb52194636ac35011
Guilherme Godoi <62507710+Glerme@users.noreply.github.com>
2020-12-08 19:34:36 -0300

    1.1

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const pkmContainer = document.querySelector("pkm-container")
+const input = document.getElementById("pesquisa")
+const pesquisar = document.getElementById("buscar")
+const nome = document.getElementById('nomeP')
+const idpkm = document.getElementById("numpkm")
 
 
-
-fetch("https://pokeapi.co/api/v2/pokemon/")
-.then(response => response.json())
-.then((pokemon) => {
-    pkmContainer.innerHTML = pokemon
+function procurarPoke(nomePok) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${nomePok}`)
+    .then(response => response.json())
+    .then((pokemon) => {
+        
+        console.log(idpkm.innerText = '#' + pokemon.id)
+        console.log(nome.innerText = pokemon.name)
+        
 })
+}
+
+pesquisar.addEventListener('click', event =>{
+    event.preventDefault()
+    const nomePok = input.value.toLowerCase()
+    procurarPoke(nomePok)
+})
+
 
 
 
