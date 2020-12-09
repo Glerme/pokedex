c7b13abe02115ba17d0ed596f48301f5b780160a
Guilherme Godoi <guiggff@gmail.com>
2020-12-09 11:35:07 -0300

    1.2

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-const pkmContainer = document.querySelector("pkm-container")
-const input = document.getElementById("pesquisa")
-const pesquisar = document.getElementById("buscar")
-const nome = document.getElementById('nomeP')
-const idpkm = document.getElementById("numpkm")
-
+const pkmContainer = document.querySelector("pkm-container");
+const input = document.getElementById("pesquisa");
+const pesquisar = document.getElementById("buscar");
+const idpkm = document.getElementById("numpkm");
+const nomePokemon = document.getElementById("nomeP");
+const habilidade = document.getElementById("habilidade");
+const type1 = document.getElementById("tipo1");
+const type2 = document.getElementById("tipo2");
+const hp = document.getElementById("hp")
+const normalImg = document.getElementById("normal")
+const shiny = document.getElementById("shiny")
 
 function procurarPoke(nomePok) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${nomePok}`)
-    .then(response => response.json())
+  fetch(`https://pokeapi.co/api/v2/pokemon/${nomePok}`)
+    .then((response) => response.json())
     .then((pokemon) => {
-        
-        console.log(idpkm.innerText = '#' + pokemon.id)
-        console.log(nome.innerText = pokemon.name)
-        
-})
+      console.log(pokemon);
+      const imagem = normalImg.src = pokemon.sprites.front_default
+      const imgShiny = shiny.src = pokemon.sprites.front_shiny
+      const idpokemon = (idpkm.innerText = "#" + pokemon.id);
+      const nomepkm = (nomePokemon.innerText = pokemon.name.toUpperCase());
+      const habilidades = (habilidade.innerText = pokemon.abilities[0].ability.name.toUpperCase());
+      const tipo1 = (type1.innerText = pokemon.types[0].type.name.toUpperCase());
+      const tipo2 =  (type2.innerText = pokemon.types[1].type.name.toUpperCase()); // Mudar dps para um tipo
+      const pv = hp.innerHTML = pokemon.stats[0].base_stat
+
+      return idpokemon, nomepkm, tipo1, habilidades,tipo2,pv,imagem,imgShiny
+    });
 }
 
-pesquisar.addEventListener('click', event =>{
-    event.preventDefault()
-    const nomePok = input.value.toLowerCase()
-    procurarPoke(nomePok)
-})
-
-
-
-
+pesquisar.addEventListener("click", (event) => {
+  event.preventDefault();
+  const nomePok = input.value.toLowerCase();
+  procurarPoke(nomePok);
+});
