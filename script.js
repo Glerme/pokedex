745d9c81fa9545268dffca93f682227f4f5c68ca
Guilherme Godoi <guiggff@gmail.com>
2020-12-11 11:52:18 -0300

    1.3

--- script.js
+++ script.js
@@ -1,35 +1,94 @@
 const pkmContainer = document.querySelector("pkm-container");
 const input = document.getElementById("pesquisa");
-const pesquisar = document.getElementById("buscar");
-const idpkm = document.getElementById("numpkm");
-const nomePokemon = document.getElementById("nomeP");
-const habilidade = document.getElementById("habilidade");
-const type1 = document.getElementById("tipo1");
-const type2 = document.getElementById("tipo2");
-const hp = document.getElementById("hp")
-const normalImg = document.getElementById("normal")
-const shiny = document.getElementById("shiny")
+const btnPesquisar = document.getElementById("buscar");
+const pkm = document.getElementById("pkm");
+const eventos = ['click', 'touchstart','submit']
 
-function procurarPoke(nomePok) {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${nomePok}`)
-    .then((response) => response.json())
-    .then((pokemon) => {
-      console.log(pokemon);
-      const imagem = normalImg.src = pokemon.sprites.front_default
-      const imgShiny = shiny.src = pokemon.sprites.front_shiny
-      const idpokemon = (idpkm.innerText = "#" + pokemon.id);
-      const nomepkm = (nomePokemon.innerText = pokemon.name.toUpperCase());
-      const habilidades = (habilidade.innerText = pokemon.abilities[0].ability.name.toUpperCase());
-      const tipo1 = (type1.innerText = pokemon.types[0].type.name.toUpperCase());
-      const tipo2 =  (type2.innerText = pokemon.types[1].type.name.toUpperCase()); // Mudar dps para um tipo
-      const pv = hp.innerHTML = pokemon.stats[0].base_stat
-
-      return idpokemon, nomepkm, tipo1, habilidades,tipo2,pv,imagem,imgShiny
-    });
+async function procurarPoke(nomePok) {
+  try {
+   return await fetch(`https://pokeapi.co/api/v2/pokemon/${nomePok}`)
+      .then((response) => {return response.json()});
+  } catch (error) {
+    console.error();
+  }
 }
 
-pesquisar.addEventListener("click", (event) => {
+function criarHtml(pokemon) {
+  const card = `
+ <section class="pkm">
+ <div class="imagens">
+   <img id="normal" src="${pokemon.sprites.front_default}"/>
+   <img id="shiny" src="${pokemon.sprites.front_shiny}">
+ </div>
+ 
+ <h3>${pokemon.name.toUpperCase()}</h3>
+ <span id="numpkm">${"#" + pokemon.id}</span>
+ <div class="tipos">
+   <p id="tipo1">${pokemon.types[0].type.name.toUpperCase()}</p>
+   <p id="tipo2">${tipo2(pokemon)}</p>
+   
+ 
+ </div>
+ 
+ <div class="container-habilidades">
+   <div class="divisao">
+     <div>
+       <span>Abilities:</span>
+       <p>${pokemon.abilities[0].ability.name.toUpperCase()}</p>
+     </div>
+     <div>
+       <span>HP:</span>
+       <p>${pokemon.stats[0].base_stat}</p>
+     </div>
+     <div>
+       <span>Attack:</span>
+       <p>${pokemon.stats[1].base_stat}</p>
+     </div>
+     <div>
+       <span>Defense:</span>
+       <p>${pokemon.stats[2].base_stat}</p>
+     </div>
+     <div>
+       <span>Sp. Atk:</span>
+       <p>${pokemon.stats[3].base_stat}</p>
+     </div>
+     <div>
+       <span>Sp. Def:</span>
+       <p>${pokemon.stats[4].base_stat}</p>
+     </div>
+     <div>
+       <span>Speed:</span>
+       <p>${pokemon.stats[5].base_stat}</p>
+     </div>
+     
+   </div>
+   
+   </div>
+ </section>
+ </div>
+   `;
+
+  return card;
+}
+
+function tipo2(pokemon) {
+  if (pokemon.types[1]) {
+    return pokemon.types[1].type.name.toUpperCase();
+  } else{
+    return " - "
+  }
+}
+
+function startApp(pokemon) {
+  pkm.innerHTML = criarHtml(pokemon);
+}
+
+
+eventos.forEach(evento => btnPesquisar.addEventListener(evento, buscar))
+
+function buscar(event) {
   event.preventDefault();
   const nomePok = input.value.toLowerCase();
-  procurarPoke(nomePok);
-});
+  procurarPoke(nomePok).then(pokemon => {startApp(pokemon)})
+  
+}
