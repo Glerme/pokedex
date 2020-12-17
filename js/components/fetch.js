68d6d07e9a1ca54c4aaa33d93e994efa3125585e
Guilherme Godoi <guiggff@gmail.com>
2020-12-17 15:13:16 -0300

    1.6

--- js/components/fetch.js
+++ js/components/fetch.js
@@ -2,10 +2,14 @@ export default async function procurarPoke(nomePok) {
     try {
       return await fetch(`https://pokeapi.co/api/v2/pokemon/${nomePok}`).then(
         (response) => {
+          const erro = document.querySelector('.erro')
+          erro.style.display = 'none'
           return response.json();
         }
       );
     } catch (error) {
+      const erro = document.querySelector('.erro')
+      erro.style.display = 'block'
       console.error();
     }
   }
